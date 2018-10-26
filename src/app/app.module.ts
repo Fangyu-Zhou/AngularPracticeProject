45e56b8b529ff9b034014f291840e3f80840087a
Fangyu-Zhou <fangyu22@ufl.edu>
2018-10-26 10:21:29 -0400

    several modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,44 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { GenreComponent } from './genre/genre.component';
 import { MovieListComponent } from './movies/movie-list.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movies/movie.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './shared/layout/header.component';
+import { FooterComponent } from './shared/layout/footer.component';
+import { AboutUsComponent } from './shared/components/about-us.component';
+import { ContactUsComponent } from './shared/components/contact-us.component';
+import { NotFoundComponent } from './shared/components/not-found.component';
+import { MovieFormComponent } from './movies/movie-form.component';
+import { MyMoviesComponent } from './movies/my-movies.component';
+import { CreateAccountComponent } from './account/create-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GenreComponent,
-    MovieListComponent
+    MovieListComponent,
+    HomeComponent,
+    MovieComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    NotFoundComponent,
+    MovieFormComponent,
+    MyMoviesComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'movies', component: MovieListComponent},
+      {path: 'movie/:id', component: MovieComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
